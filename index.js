a8f6d98005d41ae96c7910c2973bbfc01b48a5ef
Mnde81 <minubis@gmail.com>
2024-07-19 11:56:10 +0300

    updated file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { Car } from "./js/Car.js";
 import { Dog } from "./js/Dog.js";
 
 const rexas = new Dog('Rexas', 'black', 3);
@@ -29,3 +30,69 @@ console.log(brisius);
 
 console.log(brisius.birthday());
 console.log(brisius.birthday());
+
+
+console.clear();
+
+
+
+const audi80 = new Car('Audi', '80', 'red', 60, 55, 8);
+console.log(audi80);
+
+console.log(audi80.turnEngineOn());
+console.log(audi80);
+
+console.log(audi80.turnEngineOn());
+console.log(audi80);
+
+console.log(audi80.turnEngineOff());
+console.log(audi80);
+
+console.log(audi80.turnEngineOff());
+console.log(audi80);
+
+console.log(audi80.turnEngineOn());
+console.log(audi80);
+
+console.log(audi80.inStartDrivingModeKm(10));
+console.log(audi80);
+
+console.log(audi80.inDrivingModeKm(100));
+console.log(audi80);
+
+console.log(audi80.stopDriving());
+console.log(audi80);
+
+console.log(audi80.fuelLeftInTankLiters());
+console.log(audi80);
+
+console.log(audi80.fillTankWithFuel(10));
+console.log(audi80);
+
+console.log(audi80.turnEngineOn());
+console.log(audi80);
+
+console.log(audi80.fillTankWithFuel(10));
+console.log(audi80);
+
+console.log(audi80.turnEngineOff());
+console.log(audi80);
+
+console.log(audi80.fillTankWithFuel(10));
+console.log(audi80);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
