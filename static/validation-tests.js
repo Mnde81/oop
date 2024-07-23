c0120b7c7cf8177c87e7a58b2e459df2d59410ce
Mnde81 <minubis@gmail.com>
2024-07-23 17:08:08 +0300

    updated file

---
+++ static/validation-tests.js
@@ -0,0 +1,10 @@
+import { IsValid } from "./isValid.js";
+
+
+
+// console.log(IsValid.username('asda'));
+// console.log(IsValid.username('    '));
+// console.log(IsValid.username(' asd as das d'));
+// console.log(IsValid.username('sasdaasdfasdfadfasfasdfasfasdfsadf'));
+// console.log(IsValid.username('@asdsfgfbasdf'));
+// console.log(IsValid.username('Česnakas'));
